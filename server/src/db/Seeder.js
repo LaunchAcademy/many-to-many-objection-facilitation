63d1395058e2b43feb63570c20bee308c9ee9af0
dovakeen118 <kegillis118@gmail.com>
2022-01-25 16:11:12 -0500

    seeder comments

--- server/src/db/Seeder.js
+++ server/src/db/Seeder.js
@@ -1,26 +1,34 @@
 /* eslint-disable no-console */
-import { connection } from "../boot.js"
+import { connection } from "../boot.js";
 
-import { Book, Author, Authorship } from "../models/index.js"
+import { Book, Author, Authorship } from "../models/index.js";
 
 class Seeder {
   static async seed() {
-    const martian = await Book.query().insertAndFetch({name: "The Martian"})
-    const kingdoms = await Book.query().insertAndFetch({name: "The Hundred Thousand Kingdoms"})
-    const fifthSeason = await Book.query().insertAndFetch({name: "The Fifth Season"})
-    const rails = await Book.query().insertAndFetch({name: "The Rails 4 Way"})
+    // Create individual Books
+    const martian = await Book.query().insertAndFetch({ name: "The Martian" });
+    const kingdoms = await Book.query().insertAndFetch({ name: "The Hundred Thousand Kingdoms" });
+    const fifthSeason = await Book.query().insertAndFetch({ name: "The Fifth Season" });
+    const rails = await Book.query().insertAndFetch({ name: "The Rails 4 Way" });
 
-    // const author1 = await Author.query.insertAndFetch({name: "Andy Weir"})
-    // const authorship1 = await Authorship.query().insertAndFetch({authorId: author1.id, bookId: martian.id })
+    // // Create individual Author
+    const author1 = await Author.query.insertAndFetch({ name: "Andy Weir"})
+    // // Create association between Book and Author (with id's)
+    const authorship1 = await Authorship.query().insertAndFetch({ authorId: author1.id, bookId: martian.id })
 
-    await martian.$relatedQuery("authors").insert({ name: "Andy Weir"})
-    await kingdoms.$relatedQuery("authors").insert({ name: "N.K. Jemisin"})
-    await rails.$relatedQuery("authors").insert({ name: "Obie Fernandez"})
-    await rails.$relatedQuery("authors").insert({ name: "Kevin Faustino"})
 
-    console.log("Done!")
-    await connection.destroy()
+
+
+    // Create Author AND associate with book! (using associations defined in relationMappings)
+    await martian.$relatedQuery("authors").insert({ name: "Andy Weir" });
+
+    await kingdoms.$relatedQuery("authors").insert({ name: "N.K. Jemisin" });
+    await rails.$relatedQuery("authors").insert({ name: "Obie Fernandez" });
+    await rails.$relatedQuery("authors").insert({ name: "Kevin Faustino" });
+
+    console.log("Done!");
+    await connection.destroy();
   }
 }
 
-export default Seeder
+export default Seeder;
